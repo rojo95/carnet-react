a775cbc71178e64a0bc1c63ca888f2ae4897b558
Johan Román <r.johan95@gmail.com>
2022-12-08 01:02:50 -0400

    cambio de estructura de archivos

---
+++ src/layouts/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../components/Navbar'
+
+const Layout =({children}) =>{
+    return(
+        <>
+            <div>
+                <Navbar/>
+                {/* <ToolBar/> */}
+                {/* <Sides/> */}
+                {/* <Backdrop/> */}
+            </div>
+            <main>{children}</main>
+            <div>
+                <h1>Pie de pagina</h1>
+            </div>
+        </>
+    )
+}
+
+export default Layout;
