94704d22f2630b5471f9f6b8b5bb6563a3b74bd0
Johan Román <r.johan95@gmail.com>
2023-01-09 04:02:19 -0400

    impresion de componente en pdf

--- src/components/carnet/index.js
+++ src/components/carnet/index.js
@@ -1,93 +1,108 @@
+import React from "react";
 import { Rnd } from "react-rnd";
 import './carnet.css'
 
-export default function Carnet({info}) {
-    const d = new Date;
-    return (
-        <div className="carnet shadow-md">
-            <Rnd
-                default={{
-                    x: 0,
-                    y: 0,
-                }}
+class Carnet extends React.Component {
+    render() {
+        const d = new Date;
+        const bold = 'font-bold';
+        const sizes = [
+            'text-2xl',
+            'text-xl',
+            'text-base',
+            'text-sm',
+            'text-xs',
+        ]
+        return (
+            <div className="carnet shadow-md">
+                <Rnd
+                    default={{
+                        x: 0,
+                        y: 0,
+                    }}
+                    bounds='parent'
+                    >
+                    <img src={this.props.info.background} width={311.811} height={481.888} />
+                </Rnd>
+                <Rnd 
+                    default={{
+                        x: 55,
+                        y: 20,
+                    }}
+                    bounds='parent'
+                    dragGrid={[20,20]}
+                    enableResizing={false}
                 >
-                <img src={info.background} width={311.811} height={481.888} />
-            </Rnd>
-            <Rnd 
-                default={{
-                    x: 55,
-                    y: 20,
-                }}
-                bounds='parent'
-                dragGrid={[20,20]}
-                enableResizing={false}
-            >
-                <p className={`nombre ${info.nameColor ? '' : 'text-white'} ${info.nameBold ? 'font-bold' : ''} text-2xl font-bold`}>
-                    {info.factory}
-                </p>
-            </Rnd>
-            <Rnd 
-                default={{
-                    x: 90,
-                    y: 80,
-                }}
-                dragGrid={[20,20]}
-                bounds='parent'
+                    <p className={`factory ${this.props.info.factoryColor ? '' : 'text-white'} ${this.props.info.factoryBold ? bold : ''} ${sizes[this.props.info.factorySize]}`}>
+                        {this.props.info.factory}
+                    </p>
+                </Rnd>
+                <Rnd 
+                    default={{
+                        x: 90,
+                        y: 80,
+                    }}
+                    dragGrid={[20,20]}
+                    bounds='parent'
+                    >
+                    <img src={this.props.info.img} className='img'/>
+                </Rnd>
+                <Rnd 
+                    default={{
+                        x: 40,
+                        y: 250,
+                    }}
+                    bounds='parent'
+                    dragGrid={[20,20]}
+                    enableResizing={false}
                 >
-                <img src={info.img} className='img'/>
-            </Rnd>
-            <Rnd 
-                default={{
-                    x: 40,
-                    y: 250,
-                }}
-                bounds='parent'
-                dragGrid={[20,20]}
-                enableResizing={false}
-            >
-                <div className={`nombre ${info.nameColor ? '' : 'text-white'} ${info.nameBold ? 'font-bold' : ''}`}>
-                    {info.name}
-                </div>
-            </Rnd>
-            <Rnd 
-                default={{
-                    x: 40,
-                    y: 270,
-                }}
-                bounds='parent'
-                dragGrid={[20,20]}
-                enableResizing={false}
-            >
-                <div className="cargo">
-                    {info.cargo}
-                </div>
-            </Rnd>
-            <Rnd 
-                default={{
-                    x: 40,
-                    y: 290,
-                }}
-                bounds='parent'
-                dragGrid={[20,20]}
-                enableResizing={false}
-            >
-                <div className="id">
-                    {`${info.labelId}: ${info.id}`}
-                </div>
-            </Rnd>
-            <Rnd 
-                default={{
-                    x: 95,
-                    y: 390,
-                }}
-                bounds='parent'
-                dragGrid={[20,20]}
-                enableResizing={false}
-            >
-                <div className={`${info.fecha ? 'hidden' : ''} ${info.fechaBold ? 'font-bold' : ''} ${info.fechaLigth ? 'text-white' : ''}`}>
-                    {`Vence: ${d.getDay()+1}/${d.getMonth()+1}/${d.getFullYear()+1}`}
-                </div>
-            </Rnd>
-        </div>
-    );
+                    <div className={`nombre ${this.props.info.nameColor ? '' : 'text-white'} ${this.props.info.nameBold ? bold : ''} ${sizes[this.props.info.nameSize]}`}>
+                        {this.props.info.name}
+                    </div>
+                </Rnd>
+                <Rnd 
+                    default={{
+                        x: 40,
+                        y: 270,
+                    }}
+                    bounds='parent'
+                    dragGrid={[20,20]}
+                    enableResizing={false}
+                >
+                    <div className={`cargo ${this.props.info.cargoColor ? '' : 'text-white'} ${this.props.info.cargoBold ? bold : ''} ${sizes[this.props.info.cargoSize]}`}>
+                        {this.props.info.cargo}
+                    </div>
+                </Rnd>
+                <Rnd 
+                    default={{
+                        x: 40,
+                        y: 290,
+                    }}
+                    bounds='parent'
+                    dragGrid={[20,20]}
+                    enableResizing={false}
+                >
+                    <div className={`id ${this.props.info.idColor ? '' : 'text-white'} ${this.props.info.idBold ? bold : ''} ${sizes[this.props.info.idSize]}`}>
+                        {`${this.props.info.labelId}: ${this.props.info.id}`}
+                    </div>
+                </Rnd>
+                <Rnd 
+                    default={{
+                        x: 95,
+                        y: 390,
+                    }}
+                    bounds='parent'
+                    dragGrid={[20,20]}
+                    enableResizing={false}
+                >
+                    <div className={`${this.props.info.fecha ? 'hidden' : ''} ${this.props.info.fechaBold ? bold : ''} ${this.props.info.fechaLigth ? 'text-white' : ''}`}>
+                        {`Vence: ${d.getDay()+1}/${d.getMonth()+1}/${d.getFullYear()+1}`}
+                    </div>
+                </Rnd>
+            </div>
+        );
+
+    }
 }
+
+export default Carnet;
--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React from "react";
 // import logo from '../../assets/logo.svg';
 import './App.css';
-import carnet from '../../assets/img/carnet.jpg';
+import carnetImg from '../../assets/img/carnet.jpg';
 
-// import { Rnd } from 'react-rnd';
 import Carnet from '../../components/carnet';
+import ReactToPrint from 'react-to-print';
 
-class App extends React.Component {
+const ref = React.createRef();
+class App extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      background: carnet,
+      background: carnetImg,
       factory: 'OVNISOLUTIONS',
+      factoryColor: true,
+      factoryBold: true,
+      factorySize: 0,
       name: '',
       nameColor: true,
       nameBold: false,
+      nameSize: 2,
       cargo: 'Gerente General',
       cargoColor: true,
       cargoBold: false,
+      cargoSize: 2,
       labelId: '',
       id: '',
+      idColor: true,
+      idBold: false,
+      idSize: 2,
       fecha: false,
       fechaLigth: true,
       fechaBold: true,
@@ -50,6 +59,10 @@ class App extends React.Component {
     })
   }
 
+  imprimir() {
+    alert('imprimido')
+  }
+
   render() {
     return (
       <div className="App">
@@ -68,21 +81,66 @@ class App extends React.Component {
           </a>
         </header> */}
   
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold text-white">
           Carnetización
         </h1>
   
-        <div className='grid grid-cols-2 sm:grid-cols-2 gap-1 pt-10'>
+        <div className='grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-1 pt-10'>
 
           <div className='container mx-auto max-w-md'>
-            <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+            <div className="bg-white shadow-xl rounded px-10 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="factory">
+                  Empresa
+                </label>
+                <div className='inline-flex'>
+                  <input 
+                    className="shadow appearance-none border border-gray-400 rounded-tl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    id="factory" 
+                    name="factory" 
+                    type="text" 
+                    placeholder="Nombre Completo"
+                    value={this.state.factory} 
+                    onChange={(e)=>this.setState({factory:e.target.value})}
+                  />
+                  <button 
+                    className="bg-orange-300 hover:bg-orange-200 text-gray-800 font-bold py-2 px-4"
+                    onClick={()=>this.setState({factoryColor: !this.state.factoryColor})}
+                  >
+                    {!this.state.factoryColor ? 'Oscurecer' : 'Aclarar'}
+                  </button>
+                  <button 
+                    className={`bg-blue-400 hover:bg-blue-300 text-white ${!this.state.factoryBold ? 'font-bold' : ''} py-2 px-4 rounded-tr`}
+                    onClick={()=>this.setState({factoryBold: !this.state.factoryBold})}
+                  >
+                    {this.state.factoryBold ? 'Normal' : 'Bold'}
+                  </button>
+                </div>
+                <div>
+                  <select 
+                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded-b shadow leading-tight focus:outline-none focus:shadow-outline"
+                    onChange={(e)=>{this.setState({factorySize: e.target.value})}}
+                    defaultValue={this.state.factorySize}
+                  >
+                    <option value={0} >Grande</option>
+                    <option value={1}>Mediano</option>
+                    <option value={2}>Normal</option>
+                    <option value={3}>Pequeño</option>
+                    <option value={4}>Muy Pequeño</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                  </div>
+                </div>
+              </div>
+              
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
                   Nombre Completo
                 </label>
                 <div className='inline-flex'>
                   <input 
-                    className="shadow appearance-none border rounded/l w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    className="shadow appearance-none border border-gray-400 rounded-tl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="name" 
                     name="name" 
                     type="text" 
@@ -91,17 +149,33 @@ class App extends React.Component {
                     onChange={(e)=>this.setState({name:e.target.value})}
                   />
                   <button 
-                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4"
+                    className="bg-orange-300 hover:bg-orange-200 text-gray-800 font-bold py-2 px-4"
                     onClick={(e)=>this.setState({nameColor: !this.state.nameColor})}
                   >
-                    {this.state.nameColor ? 'Oscurecer' : 'Aclarar'}
+                    &nbsp;{!this.state.nameColor ? 'Oscurecer' : 'Aclarar'}&nbsp;
                   </button>
                   <button 
-                    className={`bg-blue-400 hover:bg-blue-300 text-gray-800 ${!this.state.nameBold ? 'font-bold' : ''} py-2 px-4 rounded-r`}
+                    className={`bg-blue-400 hover:bg-blue-300 text-white ${!this.state.nameBold ? 'font-bold' : ''} py-2 px-4 rounded-tr`}
                     onClick={(e)=>this.setState({nameBold: !this.state.nameBold})}
                   >
                     {this.state.nameBold ? 'Normal' : 'Bold'}
                   </button>
+                </div>
+                <div>
+                  <select 
+                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded-b shadow leading-tight focus:outline-none focus:shadow-outline"
+                    onChange={(e)=>{this.setState({nameSize: e.target.value})}}
+                    defaultValue={this.state.nameSize}
+                  >
+                    <option value={0}>Grande</option>
+                    <option value={1}>Mediano</option>
+                    <option value={2}>Normal</option>
+                    <option value={3}>Pequeño</option>
+                    <option value={4}>Muy Pequeño</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                  </div>
                 </div>
               </div>
   
@@ -111,7 +185,7 @@ class App extends React.Component {
                 </label>
                 <div className='inline-flex'>
                   <input 
-                    className="shadow appearance-none border rounded-l w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    className="shadow appearance-none border border-gray-400 rounded-tl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="cargo" 
                     name="cargo" 
                     type="text" 
@@ -120,26 +194,43 @@ class App extends React.Component {
                     onChange={(e)=>this.setState({cargo:e.target.value})}
                   />
                   <button 
-                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4"
-                    onClick={(e)=>this.setState({nameColor: !this.state.nameColor})}
+                    className="bg-orange-300 hover:bg-orange-200 text-gray-800 font-bold py-2 px-4"
+                    onClick={(e)=>this.setState({cargoColor: !this.state.cargoColor})}
                   >
-                    {this.state.nameColor ? 'Oscurecer' : 'Aclarar'}
+                    &nbsp;{!this.state.cargoColor ? 'Oscurecer' : 'Aclarar'}&nbsp;
                   </button>
                   <button 
-                    className={`bg-blue-400 hover:bg-blue-300 text-gray-800 ${!this.state.nameBold ? 'font-bold' : ''} py-2 px-4 rounded-r`}
-                    onClick={(e)=>this.setState({nameBold: !this.state.nameBold})}
+                    className={`bg-blue-400 hover:bg-blue-300 text-white ${!this.state.cargoBold ? 'font-bold' : ''} py-2 px-4 rounded-tr`}
+                    onClick={(e)=>this.setState({cargoBold: !this.state.cargoBold})}
                   >
-                    {this.state.nameBold ? 'Normal' : 'Bold'}
+                    {this.state.cargoBold ? 'Normal' : 'Bold'}
                   </button>
+                </div>
+
+                <div>
+                  <select 
+                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded-b shadow leading-tight focus:outline-none focus:shadow-outline"
+                    onChange={(e)=>{this.setState({cargoSize: e.target.value})}}
+                    defaultValue={this.state.cargoSize}
+                  >
+                    <option value={0}>Grande</option>
+                    <option value={1}>Mediano</option>
+                    <option value={2}>Normal</option>
+                    <option value={3}>Pequeño</option>
+                    <option value={4}>Muy Pequeño</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                  </div>
                 </div>
               </div>
   
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="labelId">
-                  Descripción de Identificación
+                  Identificación
                 </label>
                 <input 
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                  className="shadow appearance-none border border-gray-400 rounded-t w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                   id="labelId" 
                   name="labelId" 
                   type="text" 
@@ -147,57 +238,103 @@ class App extends React.Component {
                   value={this.state.labelId} 
                   onChange={(e)=>this.setState({labelId:e.target.value})}
                 />
-              </div>
-  
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="id">
-                  Descripción de Identificación
-                </label>
-                <input 
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                  id="id" 
-                  name="id" 
-                  type="text" 
-                  placeholder="Número de Identificación"
-                  value={this.state.id} 
-                  onChange={(e)=>this.setState({id:e.target.value})}
-                />
-              </div>
-  
-              <div className="md:flex md:items-center mb-6">
+
                 <div className='inline-flex'>
+                  <input 
+                    className="shadow appearance-none border border-gray-400 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    id="id" 
+                    name="id" 
+                    type="text" 
+                    placeholder="Número de Identificación"
+                    value={this.state.id} 
+                    onChange={(e)=>this.setState({id:e.target.value})}
+                  />
+
                   <button 
-                    className={`shadow ${!this.state.fecha ? 'bg-purple-500 hover:bg-purple-400' : 'bg-gray-400 hover:bg-gray-300'}  focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-l`}
+                    className="bg-orange-300 hover:bg-orange-200 text-gray-800 font-bold py-2 px-4"
+                    onClick={(e)=>this.setState({idColor: !this.state.idColor})}
+                  >
+                    &nbsp;{!this.state.idColor ? 'Oscurecer' : 'Aclarar'}&nbsp;
+                  </button>
+                  <button 
+                    className={`bg-blue-400 hover:bg-blue-300 text-white ${!this.state.idBold ? 'font-bold' : ''} py-2 px-4`}
+                    onClick={(e)=>this.setState({idBold: !this.state.idBold})}
+                  >
+                    {this.state.idBold ? 'Normal' : 'Bold'}
+                  </button>
+
+                </div>
+
+                <div>
+                  <select 
+                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded-b shadow leading-tight focus:outline-none focus:shadow-outline"
+                    onChange={(e)=>{this.setState({idSize: e.target.value})}}
+                    defaultValue={this.state.idSize}
+                  >
+                    <option value={0}>Grande</option>
+                    <option value={1}>Mediano</option>
+                    <option value={2}>Normal</option>
+                    <option value={3}>Pequeño</option>
+                    <option value={4}>Muy Pequeño</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                  </div>
+                </div>
+                
+              </div>
+  
+              <div className="md:flex md:items-center mb-6 w-full">
+                <div className='inline-flex w-full'>
+                  <button 
+                    className={`w-full text-sm shadow ${!this.state.fecha ? 'bg-purple-500 hover:bg-purple-400' : 'bg-gray-400 hover:bg-gray-300'}  focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-l`}
                     type="button"
                     onClick={(e)=>this.setState({fecha:!this.state.fecha})}
                   >
                     {this.state.fecha ? 'Activar' : 'Desactivar'} Fecha
                   </button>
                   <button 
-                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4"
+                    className="bg-orange-300 hover:bg-orange-200 text-gray-800 font-bold py-2 px-4"
                     onClick={(e)=>this.setState({fechaLigth: !this.state.fechaLigth})}
                   >
-                    {this.state.fechaLigth ? 'Oscurecer' : 'Aclarar'}
+                    &nbsp;{this.state.fechaLigth ? 'Oscurecer' : 'Aclarar'}&nbsp;
                   </button>
                   <button 
-                    className={`bg-blue-400 hover:bg-blue-300 text-gray-800 ${!this.state.fechaBold ? 'font-bold' : ''} py-2 px-4 rounded-r`}
+                    className={`bg-blue-400 hover:bg-blue-300 text-white ${!this.state.fechaBold ? 'font-bold' : ''} py-2 px-4 rounded-r`}
                     onClick={(e)=>this.setState({fechaBold: !this.state.fechaBold})}
                   >
                     {this.state.fechaBold ? 'Normal' : 'Bold'}
                   </button>
                 </div>
               </div>
+
+              <div>
+              
+              </div>
   
               <div className="flex items-center justify-between">
-                <button className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                  Imprimir
-                </button>
+                <ReactToPrint
+                  trigger={() => {
+                    return <button 
+                      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                      type="button"
+                      >
+                      Imprimir
+                    </button>;
+                  }}
+                  pageStyle="@page { size : A5 ; margin: 0 0 0 0;}"
+                  documentTitle={`Carnet de ${this.state.name}`}
+                  content={() => this.ref}
+                />
               </div>
             </div>
           </div> 
 
           <div>
-            <Carnet info={this.state}/>
+            <Carnet 
+              info={this.state}
+              ref={el => (this.ref = el)}
+            />
           </div>
         </div>
 
